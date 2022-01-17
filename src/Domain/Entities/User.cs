9cb86c4303f940a5863e9eea67b0d799a28892a1
Emmy <hello@emmysteven.com>
2022-01-18 00:02:04 +0100

    add user entity

---
+++ src/Domain/Entities/User.cs
@@ -0,0 +1,35 @@
+using Checklist.Domain.Common;
+using Checklist.Domain.Enums;
+
+namespace Checklist.Domain.Entities;
+public class User : AuditableEntity
+{
+    public User()
+    {
+        // Shops = new HashSet<Shop>();
+        // Bookings = new HashSet<Booking>();
+        // IsVerified = Verified.HasValue || PasswordReset.HasValue;
+    }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public DateTime? PasswordReset { get; set; }
+    public string PhoneNumber { get; set; }
+    public Roles Role { get; set; }
+
+    public bool AcceptTerms { get; set; }
+    public string VerificationToken { get; set; }
+    public string ResetToken { get; set; }
+    public DateTime? ResetTokenExpires { get; set; }
+    public List<RefreshToken> RefreshTokens { get; set; }
+    public DateTime? Verified { get; set; }
+    public bool IsVerified => Verified.HasValue || PasswordReset.HasValue;
+    // public ICollection<Shop> Shops { get; set; }
+    // public ICollection<Booking> Bookings { get; set; }
+
+    public bool OwnsToken(string token)
+    {
+        return RefreshTokens?.Find(x => x.Token == token) != null;
+    }
+}
