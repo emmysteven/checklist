9f162298dc107366ba07c5ddb1bbe23854733c58
Emmy <hello@emmysteven.com>
2022-01-18 23:52:58 +0100

    add interface for base repository

---
+++ src/Application/Common/Interfaces/IBaseRepository.cs
@@ -0,0 +1,24 @@
+using System.Linq.Expressions;
+
+namespace Checklist.Application.Common.Interfaces;
+
+public interface IBaseRepository<T> where T : class
+{
+    Task<T> GetByIdAsync(int id);
+        
+    Task<List<T>> GetAllAsync();
+
+    Task<List<T>> GetPagedResponseAsync(int pageNumber, int pageSize);
+
+    Task<T> FindAsync(Expression<Func<T, bool>> predicate);
+
+    Task<T> CreateAsync(T entity);
+
+    Task<T> UpdateAsync(T entity);
+
+    Task<T> DeleteAsync(T entity);
+
+    Task<int> CountAsync();
+
+    Task<int> CountWhereAsync(Expression<Func<T, bool>> predicate);
+}
