a181ad795491aaef15a2989ed56d8566634bfbb7
Emmy <hello@emmysteven.com>
2022-01-18 02:31:33 +0100

    fix(domain): add id to class

--- src/Domain/Common/AuditableEntity.cs
+++ src/Domain/Common/AuditableEntity.cs
@@ -1,12 +1,9 @@
 namespace Checklist.Domain.Common;
 
-public abstract class AuditableEntity
+public class AuditableEntity
 {
+    public int Id { get; set; }
     public DateTime Created { get; set; }
-
-    public string? CreatedBy { get; set; }
-
     public DateTime? LastModified { get; set; }
-
-    public string? LastModifiedBy { get; set; }
+    public string LastModifiedBy { get; set; }
 }
