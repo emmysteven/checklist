a1c6a76de6504b1bf4e4cce1b15501c0de67a6a5
Emmy <hello@emmysteven.com>
2022-01-19 02:47:46 +0100

    add dto to handle authentication response

---
+++ src/Application/DTOs/Account/AuthResponse.cs
@@ -0,0 +1,9 @@
+using System.Text.Json.Serialization;
+
+namespace Checklist.Application.DTOs.Account;
+
+public class AuthResponse
+{
+    public string Token { get; set; }
+    [JsonIgnore] public string RefreshToken { get; set; }
+}
