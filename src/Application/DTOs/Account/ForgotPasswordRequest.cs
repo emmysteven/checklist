588c8a2b4eb88126a17b71666668c11f72afaa01
Emmy <hello@emmysteven.com>
2022-01-19 02:48:52 +0100

    add dto to handle forgot password request

---
+++ src/Application/DTOs/Account/ForgotPasswordRequest.cs
@@ -0,0 +1,8 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Checklist.Application.DTOs.Account;
+
+public class ForgotPasswordRequest
+{
+    [Required] [EmailAddress] public string Email { get; set; }
+}
