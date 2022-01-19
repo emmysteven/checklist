cb584b6e6c249117ca5f4498595d141a30dd21fe
Emmy <hello@emmysteven.com>
2022-01-19 02:49:31 +0100

    add dto to handle registration request

---
+++ src/Application/DTOs/Account/RegisterRequest.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using Checklist.Domain.Enums;
+
+namespace Checklist.Application.DTOs.Account;
+
+public class RegisterRequest
+{
+    [Required] public string FirstName { get; set; }
+
+    [Required] public string LastName { get; set; }
+
+    [Required, EmailAddress] public string Email { get; set; }
+
+    [Required] public string PhoneNumber { get; set; }
+
+    public Roles Role { get; set; }
+
+    [Required, MinLength(8)] public string Password { get; set; }
+
+    // [Required]
+    // [Compare("Password")]
+    // public string ConfirmPassword { get; set; }
+}
