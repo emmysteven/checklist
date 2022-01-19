65d100ca80638983322cc577e563ed0d333dc5e9
Emmy <hello@emmysteven.com>
2022-01-19 02:41:34 +0100

    add class to handle api exceptions

---
+++ src/Application/Common/Exceptions/ApiException.cs
@@ -0,0 +1,15 @@
+using System.Globalization;
+
+namespace Checklist.Application.Common.Exceptions;
+
+public class ApiException : Exception
+{
+    public ApiException() : base() { }
+
+    public ApiException(string message) : base(message) { }
+
+    public ApiException(string message, params object[] args)
+        : base(String.Format(CultureInfo.CurrentCulture, message, args))
+    {
+    }
+}
