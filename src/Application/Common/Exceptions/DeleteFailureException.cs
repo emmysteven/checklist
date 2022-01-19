5fc0dda8779488a4583fec81a9809a7c6244389a
Emmy <hello@emmysteven.com>
2022-01-19 02:42:33 +0100

    add class to handle delete failure exceptions

---
+++ src/Application/Common/Exceptions/DeleteFailureException.cs
@@ -0,0 +1,9 @@
+namespace Checklist.Application.Common.Exceptions;
+
+public class DeleteFailureException : Exception
+{
+    public DeleteFailureException(string name, object key, string message)
+        : base($"Deletion of entity \"{name}\" ({key}) failed. {message}")
+    {
+    }
+}
