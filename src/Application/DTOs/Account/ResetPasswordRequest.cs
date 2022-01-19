9859625ef48a584925af04874f531cc58c22d0e4
Emmy <hello@emmysteven.com>
2022-01-19 02:50:08 +0100

    add dto to handle reset password request

---
+++ src/Application/DTOs/Account/ResetPasswordRequest.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Checklist.Application.DTOs.Account;
+
+public class ResetPasswordRequest
+{
+    [Required] [EmailAddress] public string Email { get; set; }
+
+    [Required] public string Token { get; set; }
+
+    [Required] [MinLength(6)] public string Password { get; set; }
+
+    [Required] [Compare("Password")] public string ConfirmPassword { get; set; }
+}
