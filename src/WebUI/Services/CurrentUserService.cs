62aff37500975e4fbfa321a28d493831531ffdd7
Emmy <hello@emmysteven.com>
2022-01-21 00:06:34 +0100

    add service for current users

---
+++ src/WebUI/Services/CurrentUserService.cs
@@ -0,0 +1,14 @@
+using Checklist.Application.Common.Interfaces;
+
+namespace Checklist.WebUI.Services;
+
+public class CurrentUserService: ICurrentUserService
+{
+    public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+    {
+        UserId = httpContextAccessor.HttpContext?.User.FindFirst("Id")?.Value;
+        Console.WriteLine($"UserId is: {UserId}");
+    }
+    public string UserId { get; }
+    public bool IsAuthenticated => UserId != null;
+}
