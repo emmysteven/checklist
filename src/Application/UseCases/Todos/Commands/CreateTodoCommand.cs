6f32f51e2bf30b81a26c4a0c1b37ad85ab9f28cc
Emmy <hello@emmysteven.com>
2022-01-20 03:52:36 +0100

    add class to create a todo

---
+++ src/Application/UseCases/Todos/Commands/CreateTodoCommand.cs
@@ -0,0 +1,30 @@
+using AutoMapper;
+using Checklist.Application.Common.Interfaces;
+using Checklist.Application.Common.Wrappers;
+using Checklist.Domain.Entities;
+using MediatR;
+
+namespace Checklist.Application.UseCases.Todos.Commands;
+public class CreateTodoCommand : IRequest<Response<int>>
+{
+    public string Name { get; set; }
+}
+    
+public class CreateTodoHandler : IRequestHandler<CreateTodoCommand, Response<int>>
+{
+    private readonly IMapper _mapper;
+    private readonly ITodoRepository _todoRepository;
+
+    public CreateTodoHandler(IMapper mapper, ITodoRepository todoRepository)
+    {
+        _mapper = mapper;
+        _todoRepository = todoRepository;
+    }
+
+    public async Task<Response<int>> Handle(CreateTodoCommand request, CancellationToken cancellationToken)
+    {
+        var todo = _mapper.Map<Todo>(request);
+        await _todoRepository.CreateAsync(todo);
+        return new Response<int>(todo.Id);
+    }
+}
