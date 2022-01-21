9f91cfebe67b189ab728b75190e40728170924d2
Emmy <hello@emmysteven.com>
2022-01-21 03:11:00 +0100

    chore(domain): suppress nullable warning with '!'

--- src/Domain/Common/AuditableEntity.cs
+++ src/Domain/Common/AuditableEntity.cs
@@ -3,7 +3,7 @@ namespace Checklist.Domain.Common;
 public class AuditableEntity
 {
     public int Id { get; set; }
-    public DateTime Created { get; set; }
+    public DateTime? Created { get; set; }
     public DateTime? LastModified { get; set; }
-    public string LastModifiedBy { get; set; }
+    public string? LastModifiedBy { get; set; }
 }
