c8ebceb182774e23f521787d76ee5ad24cb7cfe9
Emmy <hello@emmysteven.com>
2022-01-21 03:11:00 +0100

    chore(domain): suppress nullable warning with '!'

--- src/Domain/Entities/Todo.cs
+++ src/Domain/Entities/Todo.cs
@@ -3,27 +3,27 @@ using Checklist.Domain.Events;
 
 namespace Checklist.Domain.Entities;
 
-public class Todo: AuditableEntity, IHasDomainEvent
+public class Todo: AuditableEntity//, IHasDomainEvent
 {
-    public string Name { get; set; }
+    public string? Name { get; set; }
     
-    public int ListId { get; set; }
-    private bool _done;
-    public bool Done
-    {
-        get => _done;
-        set
-        {
-            if (value && _done == false)
-            {
-                DomainEvents.Add(new TodoListCompletedEvent(this));
-            }
-    
-            _done = value;
-        }
-    }
+    // public int ListId { get; set; }
+    // private bool _done;
+    // public bool Done
+    // {
+    //     get => _done;
+    //     set
+    //     {
+    //         if (value && _done == false)
+    //         {
+    //             DomainEvents.Add(new TodoListCompletedEvent(this));
+    //         }
+    //
+    //         _done = value;
+    //     }
+    // }
     
     public TodoList TodoList { get; set; } = null!;
 
-    public List<DomainEvent> DomainEvents { get; set; } = new List<DomainEvent>();
+    // public List<DomainEvent> DomainEvents { get; set; } = new List<DomainEvent>();
 }
--- src/Domain/Entities/User.cs
+++ src/Domain/Entities/User.cs
@@ -10,19 +10,19 @@ public class User : AuditableEntity
         // Bookings = new HashSet<Booking>();
         // IsVerified = Verified.HasValue || PasswordReset.HasValue;
     }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+    public string? Password { get; set; }
     public DateTime? PasswordReset { get; set; }
-    public string PhoneNumber { get; set; }
+    public string? PhoneNumber { get; set; }
     public Roles Role { get; set; }
 
     public bool AcceptTerms { get; set; }
-    public string VerificationToken { get; set; }
-    public string ResetToken { get; set; }
+    public string? VerificationToken { get; set; }
+    public string? ResetToken { get; set; }
     public DateTime? ResetTokenExpires { get; set; }
-    public List<RefreshToken> RefreshTokens { get; set; }
+    public List<RefreshToken>? RefreshTokens { get; set; }
     public DateTime? Verified { get; set; }
     public bool IsVerified => Verified.HasValue || PasswordReset.HasValue;
     // public ICollection<Shop> Shops { get; set; }
