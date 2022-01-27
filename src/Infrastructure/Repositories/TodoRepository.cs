04b72d4f9a95afe1b001983f3fa638e40d96edfa
Emmy <hello@emmysteven.com>
2022-01-27 23:53:23 +0100

    build(infrastructure): implement repository for Todo

--- src/Infrastructure/Repositories/TodoRepository.cs
+++ src/Infrastructure/Repositories/TodoRepository.cs
@@ -7,10 +7,10 @@ namespace Checklist.Infrastructure.Repositories;
 
 public class TodoRepository : BaseRepository<Todo>, ITodoRepository
 {
-    private readonly DbSet<Todo> _item;
+    private readonly DbSet<Todo> _todo;
 
     public TodoRepository(DataContext context) : base(context)
     {
-        _item = context.Set<Todo>();
+        _todo = context.Set<Todo>();
     }
 }
