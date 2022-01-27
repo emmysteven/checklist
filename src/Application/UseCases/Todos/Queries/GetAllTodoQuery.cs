45ec02c24460474f81e542a94b0f7875c364b080
Emmy <hello@emmysteven.com>
2022-01-27 23:54:14 +0100

    build(application): implement query to get all todos

--- src/Application/UseCases/Todos/Queries/GetAllTodoQuery.cs
+++ src/Application/UseCases/Todos/Queries/GetAllTodoQuery.cs
@@ -13,20 +13,20 @@ public class GetAllTodoQuery : IRequest<PagedResponse<IEnumerable<GetAllTodoVm>>
     
 public class GetAllTodoHandler : IRequestHandler<GetAllTodoQuery, PagedResponse<IEnumerable<GetAllTodoVm>>>
 {
-    private readonly ITodoRepository _shopRepository;
+    private readonly ITodoRepository _todoRepository;
     private readonly IMapper _mapper;
 
     public GetAllTodoHandler(ITodoRepository todoRepository, IMapper mapper)
     {
-        _shopRepository = todoRepository;
+        _todoRepository = todoRepository;
         _mapper = mapper;
     }
 
     public async Task<PagedResponse<IEnumerable<GetAllTodoVm>>> Handle(GetAllTodoQuery request, CancellationToken cancellationToken)
     {
         var filter = _mapper.Map<GetAllTodoParameter>(request);
-        var product = await _shopRepository.GetPagedResponseAsync(filter.PageNumber, filter.PageSize);
-        var todoViewModel = _mapper.Map<IEnumerable<GetAllTodoVm>>(product);
+        var todo = await _todoRepository.GetPagedResponseAsync(filter.PageNumber, filter.PageSize);
+        var todoViewModel = _mapper.Map<IEnumerable<GetAllTodoVm>>(todo);
         return new PagedResponse<IEnumerable<GetAllTodoVm>>(todoViewModel, filter.PageNumber, filter.PageSize); 
     }
 }
