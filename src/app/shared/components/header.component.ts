6afa5da4c00df556d9c732f5fa725b21b5f8c222
Emmy <hello@emmysteven.com>
2022-04-15 21:35:01 +0100

    feat: add header component

--- src/app/shared/components/header.component.ts
+++ src/app/shared/components/header.component.ts
@@ -1,20 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 
+import {AuthService} from "../../core/services/auth.service";
+
 @Component({
   selector: 'app-header',
   template: `
-    <p>
-      header works!
-    </p>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <a class="navbar-brand" routerLink="/">Checklist</a>
+      <button
+        class="navbar-toggler"
+        type="button" data-toggle="collapse"
+        data-target="#navbarDefault"
+        aria-controls="navbarDefault"
+        aria-expanded="false"
+        aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarDefault">
+        <ul class="navbar-nav ml-auto">
+          <li *ngIf="!isLoggedIn()" class="nav-item">
+            <a class="nav-link" routerLink="/login">Login</a>
+          </li>
+          <li *ngIf="!isLoggedIn()" class="nav-item">
+            <a class="nav-link" routerLink="/register">Register</a>
+          </li>
+          <li *ngIf="isLoggedIn()" class="nav-item">
+            <a class="nav-link" role="button" (click)="logout()">logout</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   `,
-  styles: [
-  ]
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  constructor(public authService: AuthService) {}
+  isLoggedIn = ():boolean => this.authService.isLoggedIn();
+  logout = ():void => this.authService.logout();
+  ngOnInit(): void {}
 
 }
+
