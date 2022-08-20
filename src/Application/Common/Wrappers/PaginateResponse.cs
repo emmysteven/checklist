c6fbeaa0d147f765c6a03377a81e0f39706d7f8c
Emmy <hello@emmysteven.com>
2022-08-20 21:59:10 +0100

    refactor: rewrite codebase

--- src/Application/Common/Wrappers/PaginateResponse.cs
+++ src/Application/Common/Wrappers/PaginateResponse.cs
@@ -2,14 +2,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Checklist.Application.Common.Wrappers;
 
-public class PaginatedList<T>
+public class PaginateResponse<T>
 {
     public List<T> Items { get; }
     public int PageNumber { get; }
     public int TotalPages { get; }
     public int TotalCount { get; }
 
-    public PaginatedList(List<T> items, int count, int pageNumber, int pageSize)
+    public PaginateResponse(List<T> items, int count, int pageNumber, int pageSize)
     {
         PageNumber = pageNumber;
         TotalPages = (int)Math.Ceiling(count / (double)pageSize);
@@ -21,11 +21,11 @@ public class PaginatedList<T>
 
     public bool HasNextPage => PageNumber < TotalPages;
 
-    public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize)
+    public static async Task<PaginateResponse<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize)
     {
         var count = await source.CountAsync();
         var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
 
-        return new PaginatedList<T>(items, count, pageNumber, pageSize);
+        return new PaginateResponse<T>(items, count, pageNumber, pageSize);
     }
 }
