6c21458d99fae535928766dbb38dbe5fb7478d01
Emmy <hello@emmysteven.com>
2022-08-20 21:48:35 +0100

    fix: make property non nullable

--- src/Domain/Entities/Todo.cs
+++ src/Domain/Entities/Todo.cs
@@ -4,25 +4,8 @@ namespace Checklist.Domain.Entities;
 
 public class Todo: AuditableEntity//, IHasDomainEvent
 {
-    public string Name { get; set; }
-    
-    // public int ListId { get; set; }
-    // private bool _done;
-    // public bool Done
-    // {
-    //     get => _done;
-    //     set
-    //     {
-    //         if (value && _done == false)
-    //         {
-    //             DomainEvents.Add(new TodoListCompletedEvent(this));
-    //         }
-    //
-    //         _done = value;
-    //     }
-    // }
+    public string Name { get; set; } = string.Empty;
     public IList<Item> Items { get; private set; } = new List<Item>();
-
 
     // public List<DomainEvent> DomainEvents { get; set; } = new List<DomainEvent>();
 }
