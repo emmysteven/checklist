c6e204116f1d384e109029672ca3e16df322fb0a
Emmy <hello@emmysteven.com>
2022-08-20 21:43:33 +0100

    refactor: made entities not nullable

--- src/Domain/Entities/Item.cs
+++ src/Domain/Entities/Item.cs
@@ -4,8 +4,23 @@ namespace Checklist.Domain.Entities;
 
 public class Item : AuditableEntity
 {
-    public int? TodoId { get; set; }
-    public Todo Todos { get; set; } = null!;
-    public string? StartTime { get; set; }
-    public string? EndTime { get; set; }
+    public int TodoId { get; set; }
+    public Todo Todo { get; set; } = null!;
+    public string StartTime { get; set; } = String.Empty;
+    public string EndTime { get; set; } = String.Empty;
+    
+    // private bool _done;
+    // public bool Done
+    // {
+    //     get => _done;
+    //     set
+    //     {
+    //         if (value == true && _done == false)
+    //         {
+    //             AddDomainEvent(new ItemCompletedEvent(this));
+    //         }
+    //
+    //         _done = value;
+    //     }
+    // }
 }
