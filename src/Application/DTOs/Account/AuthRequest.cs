5a26732246370b007286a5154bdc4bea6fcfadbd
Emmy <hello@emmysteven.com>
2022-08-21 01:36:52 +0100

    fix: make properties non nullable

--- src/Application/DTOs/Account/AuthRequest.cs
+++ src/Application/DTOs/Account/AuthRequest.cs
@@ -2,6 +2,6 @@ namespace Checklist.Application.DTOs.Account;
 
 public class AuthRequest
 {
-    public string? Email { get; set; }
-    public string? Password { get; set; }
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
 }
