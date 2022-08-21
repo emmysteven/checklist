965f784a3b158f6e344ee0f4cb1279db2a3217f3
Emmy <hello@emmysteven.com>
2022-08-21 01:34:38 +0100

    refactor: rename PaginateResponse to PaginatedResponse

--- src/Application/Mappings/MappingExtensions.cs
+++ src/Application/Mappings/MappingExtensions.cs
@@ -7,8 +7,8 @@ namespace Checklist.Application.Mappings;
 
 public static class MappingExtensions
 {
-    public static Task<PaginateResponse<TDestination>> PaginatedListAsync<TDestination>(this IQueryable<TDestination> queryable, int pageNumber, int pageSize) where TDestination : class
-        => PaginateResponse<TDestination>.CreateAsync(queryable.AsNoTracking(), pageNumber, pageSize);
+    public static Task<PaginatedResponse<TDestination>> PaginatedResponseAsync<TDestination>(this IQueryable<TDestination> queryable, int pageNumber, int pageSize) where TDestination : class
+        => PaginatedResponse<TDestination>.CreateAsync(queryable.AsNoTracking(), pageNumber, pageSize);
 
     public static Task<List<TDestination>> ProjectToListAsync<TDestination>(this IQueryable queryable, IConfigurationProvider configuration) where TDestination : class
         => queryable.ProjectTo<TDestination>(configuration).AsNoTracking().ToListAsync();
