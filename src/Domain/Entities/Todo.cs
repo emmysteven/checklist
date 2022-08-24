be16f8c78947040b7e8d1579a2db290c02234b00
Emmy <hello@emmysteven.com>
2022-08-24 23:57:02 +0100

    refactor: remove redundant code

--- src/Domain/Entities/Todo.cs
+++ src/Domain/Entities/Todo.cs
@@ -5,7 +5,7 @@ namespace Checklist.Domain.Entities;
 public class Todo: AuditableEntity//, IHasDomainEvent
 {
     public string Name { get; set; } = string.Empty;
-    public IList<Item> Items { get; private set; } = new List<Item>();
+    public IList<Item> Items { get; } = new List<Item>();
 
     // public List<DomainEvent> DomainEvents { get; set; } = new List<DomainEvent>();
 }
