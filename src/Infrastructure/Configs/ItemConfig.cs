543437022bf7b9ffdf8f92a61cd6f90af018e695
Emmy <hello@emmysteven.com>
2022-08-24 23:58:32 +0100

    define relationship between item and todo

--- src/Infrastructure/Configs/ItemConfig.cs
+++ src/Infrastructure/Configs/ItemConfig.cs
@@ -8,9 +8,6 @@ public class ItemConfig : IEntityTypeConfiguration<Item>
 {
     public void Configure(EntityTypeBuilder<Item> builder)
     {
-        // builder.HasKey(s => s.Id);
-        builder.ToTable("Items");
-        builder.HasKey(i => i.Id);
-        builder.OwnsOne(i => i.Todo.Name);
+        builder.HasOne(x => x.Todo);
     }
 }
