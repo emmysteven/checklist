9d830dbd02e2b220683fb3c06c1671dd228d9157
Emmy <hello@emmysteven.com>
2022-08-27 03:33:57 +0100

    refactor: rename GetItemsQuery to GetItemQuery

--- src/Application/UseCases/Items/Queries/GetItemQuery.cs
+++ src/Application/UseCases/Items/Queries/GetItemQuery.cs
@@ -5,26 +5,26 @@ using MediatR;
 
 namespace Checklist.Application.UseCases.Items.Queries;
 
-public record GetItemsQuery : IRequest<PaginatedResponse<IEnumerable<ItemVm>>>
+public record GetItemQuery : IRequest<PaginatedResponse<IEnumerable<ItemVm>>>
 {
     public int TodoId { get; init; }
     public int PageNumber { get; init; }
     public int PageSize { get; init; } 
 }
 
-public class GetItemsHandler : IRequestHandler<GetItemsQuery, PaginatedResponse<IEnumerable<ItemVm>>>
+public class GetItemHandler : IRequestHandler<GetItemQuery, PaginatedResponse<IEnumerable<ItemVm>>>
 {
     private readonly IItemRepository _repo;
     private readonly IMapper _mapper;
 
-    public GetItemsHandler(IItemRepository repo, IMapper mapper)
+    public GetItemHandler(IItemRepository repo, IMapper mapper)
     {
         _repo = repo;
         _mapper = mapper;
     }
     
     
-    public async Task<PaginatedResponse<IEnumerable<ItemVm>>> Handle(GetItemsQuery request, CancellationToken cancellationToken)
+    public async Task<PaginatedResponse<IEnumerable<ItemVm>>> Handle(GetItemQuery request, CancellationToken cancellationToken)
     {
         var filter = _mapper.Map<ItemParameter>(request);
         var item = await _repo.GetPagedResponseAsync(filter.PageNumber, filter.PageSize);
