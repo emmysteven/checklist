2d8fc6ed57be7c8713e55d3d467d107481548fb2
Emmy <hello@emmysteven.com>
2022-08-28 21:31:12 +0100

    fix: take out unnecessary parameter

--- src/Application/Common/Wrappers/PaginatedResponse.cs
+++ src/Application/Common/Wrappers/PaginatedResponse.cs
@@ -1,31 +1,18 @@
-using Microsoft.EntityFrameworkCore;
-
 namespace Checklist.Application.Common.Wrappers;
 
 public class PaginatedResponse<T>
 {
-    public List<T> Items { get; }
+    public T Data { get; }
     public int PageNumber { get; }
     public int TotalPages { get; }
-    public int TotalCount { get; }
 
-    public PaginatedResponse(List<T> items, int count, int pageNumber, int pageSize)
+    public PaginatedResponse(T data, int pageNumber, int pageSize)
     {
         PageNumber = pageNumber;
-        TotalPages = (int)Math.Ceiling(count / (double)pageSize);
-        TotalCount = count;
-        Items = items;
+        TotalPages = (int)Math.Ceiling((double)pageSize);
+        Data = data;
     }
 
     public bool HasPreviousPage => PageNumber > 1;
-
     public bool HasNextPage => PageNumber < TotalPages;
-
-    public static async Task<PaginatedResponse<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize)
-    {
-        var count = await source.CountAsync();
-        var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
-
-        return new PaginatedResponse<T>(items, count, pageNumber, pageSize);
-    }
 }
