a341124cf50b3bf912c3be88186a85e940aec3bd
emmysteven <hello@emmysteven.com>
2023-04-16 23:49:02 +0100

    feat: add routing for auth module

---
+++ src/app/modules/auth/auth-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./login.component";
+import {SignupComponent} from "./signup.component";
+import {LayoutComponent} from "./layout.component";
+
+const routes: Routes = [
+  {
+    path: '', component: LayoutComponent,
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: SignupComponent }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
