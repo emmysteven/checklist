dce15f3ba8ed54231da2dced7b16b5a04ac431dc
emmysteven <hello@emmysteven.com>
2023-04-16 23:44:44 +0100

    style: move home module to modules folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared';
 import { AppComponent } from './app.component';
+import { AuthModule } from "./modules/auth";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AuthModule,
     SharedModule,
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
-    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,18 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
-import { AboutComponent } from './about.component';
 import { ContactComponent } from './contact.component';
+import { AboutComponent } from './about.component';
 
 
 @NgModule({
-  declarations: [
-    AboutComponent,
-    ContactComponent
-  ],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  declarations: [AboutComponent, ContactComponent],
+  imports: [CommonModule, HomeRoutingModule]
 })
 export class HomeModule { }
