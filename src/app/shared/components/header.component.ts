a1f8a39e0563d5a76b562cf7b5f07cba9964da6e
emmysteven <hello@emmysteven.com>
2023-04-17 10:24:09 +0100

    refactor: shorten import by using custom alias

--- src/app/shared/components/header.component.ts
+++ src/app/shared/components/header.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import {AuthService} from "../../core/services/auth.service";
+import { AuthService } from "@app/core/services";
 
 @Component({
   selector: 'app-header',
