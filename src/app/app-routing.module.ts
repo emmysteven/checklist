bfde60b4940cd17781dc31d259d333fc2b2fcf91
emmysteven <hello@emmysteven.com>
2023-04-17 09:51:02 +0100

    style: add extra whitespace

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-// const authModule = () => import('./auth').then(x => x.AuthModule);
-// const homeModule = () => import('./home').then(x => x.HomeModule);
+// const authModule = () => import('./modules/auth').then(x => x.AuthModule);
+// const homeModule = () => import('./modules/home').then(x => x.HomeModule);
 
 
 const routes: Routes = [
