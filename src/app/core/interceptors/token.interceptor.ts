d0a00bfd51e9c68875f47b59c778e9c193c7c44c
emmysteven <hello@emmysteven.com>
2023-04-18 12:04:15 +0100

    style: add a token interceptor

---
+++ src/app/core/interceptors/token.interceptor.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpRequest,
+  HttpInterceptor,
+  HttpErrorResponse,
+  HTTP_INTERCEPTORS
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    const localToken = localStorage.getItem('token');
+    request = request.clone({ headers: request.headers.set('Authorization', 'bearer ' + localToken)});
+    return next.handle(request);
+  }
+}
+
+export const tokenInterceptorProvider = [
+  { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
+];
