7e01b9017144c09fc37a67a4500f06927dcf6a51
emmysteven <hello@emmysteven.com>
2023-04-19 23:50:56 +0100

    refactor: remove reference to Items

--- src/Application/UseCases/Todos/Queries/TodoVm.cs
+++ src/Application/UseCases/Todos/Queries/TodoVm.cs
@@ -6,14 +6,6 @@ namespace Checklist.Application.UseCases.Todos.Queries;
 
 public class TodoVm : IMapFrom<Todo>
 {
-    public TodoVm()
-    {
-        Items = new List<ItemVm>();
-    }
-
     public int Id { get; set; }
-
     public string? Name { get; set; }
-
-    public IList<ItemVm> Items { get; set; }
 }
