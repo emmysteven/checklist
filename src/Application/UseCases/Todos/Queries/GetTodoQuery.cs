07869ebf0970b95448c912fa4ba56c02b78c0309
emmysteven <hello@emmysteven.com>
2023-04-19 23:53:05 +0100

    refactor: replace paginated response with non paginated response

--- src/Application/UseCases/Todos/Queries/GetTodoQuery.cs
+++ src/Application/UseCases/Todos/Queries/GetTodoQuery.cs
@@ -1,20 +1,16 @@
 using AutoMapper;
-using AutoMapper.QueryableExtensions;
 using Checklist.Application.Common.Interfaces;
 using Checklist.Application.Common.Wrappers;
-using Checklist.Application.Mappings;
 using MediatR;
 
 namespace Checklist.Application.UseCases.Todos.Queries;
 
-public record GetTodoQuery : IRequest<PaginatedResponse<IEnumerable<TodoVm>>>
+public record GetTodoQuery : IRequest<Response<IEnumerable<TodoVm>>>
 {
     public int Id { get; init; }
-    public int PageNumber { get; init; }
-    public int PageSize { get; init; }
 }
     
-public class GetAllTodoHandler : IRequestHandler<GetTodoQuery, PaginatedResponse<IEnumerable<TodoVm>>>
+public class GetAllTodoHandler : IRequestHandler<GetTodoQuery, Response<IEnumerable<TodoVm>>>
 {
     private readonly ITodoRepository _repo;
     private readonly IMapper _mapper;
@@ -25,12 +21,11 @@ public class GetAllTodoHandler : IRequestHandler<GetTodoQuery, PaginatedResponse
         _mapper = mapper;
     }
 
-    public async Task<PaginatedResponse<IEnumerable<TodoVm>>>  Handle(GetTodoQuery request, CancellationToken cancellationToken)
+    public async Task<Response<IEnumerable<TodoVm>>>  Handle(GetTodoQuery request, CancellationToken cancellationToken)
     {
-        var filter = _mapper.Map<TodoParameter>(request);
-        var todo = await _repo.GetPagedResponseAsync(filter.PageNumber, filter.PageSize);
+        var todo = await _repo.GetAllAsync();
         var todoVm = _mapper.Map<IEnumerable<TodoVm>>(todo);
         
-        return new PaginatedResponse<IEnumerable<TodoVm>>(todoVm, filter.PageNumber, filter.PageSize); 
+        return new Response<IEnumerable<TodoVm>>(todoVm); 
     }
 }
