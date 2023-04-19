4be406e02b49f8b88875a7402ec806c4aaacf0d9
emmysteven <hello@emmysteven.com>
2023-04-19 23:43:31 +0100

    refactor: remove reference to Items

--- src/Domain/Entities/Todo.cs
+++ src/Domain/Entities/Todo.cs
@@ -5,7 +5,6 @@ namespace Checklist.Domain.Entities;
 public class Todo: AuditableEntity//, IHasDomainEvent
 {
     public string Name { get; set; } = string.Empty;
-    public IList<Item> Items { get; } = new List<Item>();
-
+    
     // public List<DomainEvent> DomainEvents { get; set; } = new List<DomainEvent>();
 }
