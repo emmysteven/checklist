eaaf184f976f3c9ccabfad4e8494afa04c7e35ac
emmysteven <hello@emmysteven.com>
2023-04-24 12:38:22 +0100

    perf: modify code to allow for bulk insert

--- src/Application/UseCases/Items/Commands/CreateItemCommand.cs
+++ src/Application/UseCases/Items/Commands/CreateItemCommand.cs
@@ -1,47 +1,34 @@
 using AutoMapper;
-using Checklist.Application.Common.Exceptions;
 using Checklist.Application.Common.Interfaces;
 using Checklist.Application.Common.Wrappers;
+using Checklist.Domain.Common;
 using Checklist.Domain.Entities;
 using MediatR;
 
 namespace Checklist.Application.UseCases.Items.Commands;
-public record CreateItemCommand : IRequest<Response<int>>
+public record CreateItemCommand : IRequest<Response<IEnumerable<int>>>
 {
-    public int TodoId { get; set; } = default;
-
-    public string StartTime { get; set; } = string.Empty;
-    public string EndTime { get; set; } = string.Empty;
+    public IEnumerable<Item> Items { get; set; } = new List<Item>();
 }
 
-public class CreateItemHandler : IRequestHandler<CreateItemCommand, Response<int>>
+public class CreateItemHandler : IRequestHandler<CreateItemCommand, Response<IEnumerable<int>>>
 {
     private readonly IItemRepository _itemRepo;
-    private readonly ITodoRepository _todoRepo;
     private readonly IMapper _mapper;
 
-    public CreateItemHandler(IItemRepository itemRepo, ITodoRepository todoRepo, IMapper mapper)
+    public CreateItemHandler(IItemRepository itemRepo, IMapper mapper)
     {
         _itemRepo = itemRepo;
-        _todoRepo = todoRepo;
         _mapper = mapper;
     }
 
-    public async Task<Response<int>> Handle(CreateItemCommand request, CancellationToken cancellationToken)
+    public async Task<Response<IEnumerable<int>>> Handle(CreateItemCommand request, CancellationToken cancellationToken)
     {
-        var todo = await _todoRepo.GetByIdAsync(request.TodoId);
-        if(todo == null) throw new ApiException("Todo Not Found.");
+        var items = _mapper.Map<IEnumerable<Item>>(request.Items).ToList();
 
-            var newItem = new Item
-        {
-            Todo = todo,
-            StartTime = request.StartTime,
-            EndTime = request.EndTime
-        };
-        
-        var item = _mapper.Map<Item>(newItem);
-        await _itemRepo.CreateAsync(item);
-        
-        return new Response<int>(item.Id);
+        await _itemRepo.BulkInsertAsync(items);
+    
+        var itemIds = items.Select(i => i.Id);
+        return new Response<IEnumerable<int>>(itemIds);
     }
 }
