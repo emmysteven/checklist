146c0180bae4f54bb5a030c3f32a14492ac5747c
emmysteven <hello@emmysteven.com>
2023-04-24 13:46:38 +0100

    refactor: total overhaul of the item component

--- src/app/modules/dashboard/add-item.component.ts
+++ src/app/modules/dashboard/add-item.component.ts
@@ -1,19 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { FormBuilder, FormArray, FormGroup, Validators } from "@angular/forms";
 import { AlertService, ItemService } from "@app/core/services";
 import { first } from "rxjs/operators";
+import { ITodo} from "@app/core/models";
 
 
 @Component({
   selector: 'app-add-item',
   templateUrl: './add-item.component.html',
-  styles: [
-  ]
+  styles: [`
+    form { padding-bottom: 100px; }
+    .form-control { width: 130px; }
+    .form-group:nth-child(1) {
+      display: flex;
+      flex-wrap: wrap;
+    }
+    /*table th{*/
+    /*  display: inline;*/
+    /*}*/
+    /*table th:nth-child(2){*/
+    /*  float: right;*/
+    /*}*/
+    /*table th:nth-child(3){*/
+    /*  float: right;*/
+    /*}*/
+  `]
 })
+
+
 export class AddItemComponent implements OnInit {
 
   form: FormGroup = new FormGroup({});
   todos: any;
+  data: any;
+  EodDate: string = '';
+
+  timeFields: FormArray<any> = new FormArray<any>([]);
+  startEndFields: FormArray<any> = new FormArray<any>([]);
+
   loading = false;
   submitted = false;
 
@@ -24,43 +48,102 @@ export class AddItemComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.form = this.formBuilder.group({});
+    this.form = this.formBuilder.group({
+      EodDate: ['', [Validators.required]],
+      timeFields: this.formBuilder.array([
+        this.formBuilder.group({
+          id: [],
+          name: [''],
+          startTime: ['']
+        })
+      ]),
+      startEndFields: this.formBuilder.array([
+        this.formBuilder.group({
+          id: [],
+          name: [''],
+          startTime: [''],
+          endTime: ['']
+        })
+      ])
+    });
 
     this.itemService.getTodos().subscribe({
       next: data => {
-        this.todos = data;
+        this.data = data;
+
+        this.data.forEach((item: ITodo) => {
+          if (item.name.startsWith('Submit')) {
+            item.startTime = "";
+            item.endTime = "";
+          } else {
+            item.startTime = "";
+          }
+        });
+
+        this.todos = this.data;
+
+        this.todos.forEach((item: ITodo) => {
+          if (item.name.startsWith('Submit')) {
+            this.startEndFields.push(this.formBuilder.group({
+              id: [item.id],
+              name: [item.name],
+              StartTime: ['', [Validators.required, Validators.maxLength(7)]],
+              EndTime: ['', [Validators.required, Validators.maxLength(7)]]
+            }));
+          } else {
+            this.timeFields.push(this.formBuilder.group({
+              id: [item.id],
+              name: [item.name],
+              StartTime: ['', [Validators.required, Validators.maxLength(7)]]
+            }));
+          }
+        });
+
+        this.form = this.formBuilder.group({
+          EodDate: ['', Validators.required],
+          timeFields: this.timeFields,
+          startEndFields: this.startEndFields
+        });
       },
       error: err => {
-        if (err.error) {
-          try {
-            const res = JSON.parse(err.error);
-            this.todos = res.message;
-          } catch {
-            this.todos = `Error with status: ${err.status} - ${err.statusText}`;
-          }
-        } else {
-          this.todos = `Error with status: ${err.status}`;
-        }
+        console.log(err);
       }
     });
-
-    console.log(this.todos);
   }
 
-  get f() { return this.form.controls; }
 
   onSubmit() {
     this.submitted = true;
     this.alertService.clear();
 
-    if (this.form.invalid) { return }
+    const data = this.form.value;
+    const allFields = [...data.startEndFields, ...data.timeFields];
+
+    //sort the objects in the array in ascending order using the ids of the objects
+    const sortedFields = allFields.sort((a, b) => a.id - b.id);
+
+    sortedFields.forEach((obj: { EodDate: string; }) => {
+      obj.EodDate = this.form.value.EodDate;
+    });
+
+    const body = {
+      items: sortedFields
+    };
+
+    console.log(body);
+
+
+    // if (this.form.invalid) { return console.log('Invalid Inputs') }
 
     this.loading = true;
-    this.itemService.addItem(this.f)
+
+    this.itemService.addItem(body)
       .pipe(first())
       .subscribe({
         next: (response) => {
           console.log(response);
+          this.alertService.success('Item added successfully', { keepAfterRouteChange: true });
+          this.loading = false;
         },
         error: (error) => {
           console.log(error);
@@ -69,6 +152,4 @@ export class AddItemComponent implements OnInit {
         }
       });
   }
-
-
 }
