3ea0cc313d21a00462c351819a37db8773305e42
emmysteven <hello@emmysteven.com>
2023-04-25 13:15:48 +0100

    feat: add component to display saved items

---
+++ src/app/modules/dashboard/get-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-get-item',
+  templateUrl: './get-item.component.html',
+  styles: [
+  ]
+})
+export class GetItemComponent {
+
+}
