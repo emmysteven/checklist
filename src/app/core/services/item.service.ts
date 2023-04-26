b1f31c0d54141bd27b57b402e1e1caf3fa228730
emmysteven <hello@emmysteven.com>
2023-04-26 16:26:31 +0100

    add url parameter to getItem method

--- src/app/core/services/item.service.ts
+++ src/app/core/services/item.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { environment } from "@env/environment";
-import { ITodo } from "@app/core/models/todo";
-import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {environment} from "@env/environment";
+import {Item, Todo} from "@app/core/models";
+import {Observable} from "rxjs";
+import {map} from "rxjs/operators";
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -18,15 +18,21 @@ export class ItemService {
 
   constructor(private http: HttpClient) { }
 
-  getTodos(): Observable<ITodo> {
-    return this.http.get<ITodo>(`${this.baseUrl}api/todo`, httpOptions)
+  getTodos(): Observable<Todo> {
+    return this.http.get<Todo>(`${this.baseUrl}api/todo`, httpOptions)
       .pipe(map((response: any) => {
-        const data = response.data
-        return data;
+        return response.data;
       }))
   }
 
   addItem(item: any) {
     return this.http.post(`${this.baseUrl}api/item`, item);
+  }
+
+  getItem(eodDate: string): Observable<Item> {
+    return this.http.get<Item>(`${this.baseUrl}api/item?eodDate=${eodDate}`, httpOptions)
+      .pipe(map((response: any) => {
+        return response.data;
+    }))
   }
 }
