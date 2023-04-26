6da584edf019297ed4633ea06e542e3346ef37e5
emmysteven <hello@emmysteven.com>
2023-04-26 16:40:55 +0100

    feat: flesh out component

--- src/app/modules/dashboard/get-item.component.ts
+++ src/app/modules/dashboard/get-item.component.ts
@@ -1,11 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AlertService, ItemService } from "@app/core/services";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import {Item, Todo} from "@app/core/models";
 
 @Component({
   selector: 'app-get-item',
   templateUrl: './get-item.component.html',
-  styles: [
-  ]
+  styles: [`
+    .table { padding-bottom: 400px; }
+  `]
 })
-export class GetItemComponent {
+
+
+export class GetItemComponent implements OnInit {
+
+  form: FormGroup = new FormGroup({});
+  data: any;
+  items: any;
+  eodDate: string = '';
+
+  loading = false;
+  submitted = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private alertService: AlertService,
+    private itemService: ItemService
+  ) { }
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      eodDate: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.alertService.clear();
+
+    const eodDate = this.form.value.eodDate;
+
+    console.log(eodDate)
+
+    if (this.form.invalid) { return console.log('Invalid Inputs') }
+
+    this.loading = true;
+
+    this.itemService.getItem(eodDate).subscribe({
+      next: data => {
+        this.loading = false
+        this.items = data
+        this.alertService.success(this.items)
+      },
+      error: err => {
+        this.loading = false
+        this.alertService.error(err)
+        console.log(err)
+      }
+    });
+  }
 
 }
