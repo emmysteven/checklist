076e8987f601a5b19bd0cac7283b12e98b257973
emmysteven <hello@emmysteven.com>
2023-04-26 15:44:06 +0100

    refactor: implement GetByDate method

--- src/Application/Common/Interfaces/IItemRepository.cs
+++ src/Application/Common/Interfaces/IItemRepository.cs
@@ -2,4 +2,7 @@ using Checklist.Domain.Entities;
 
 namespace Checklist.Application.Common.Interfaces;
 
-public interface IItemRepository : IBaseRepository<Item> { }
+public interface IItemRepository : IBaseRepository<Item>
+{
+    Task<List<Item>> GetByDate(string eodDate);
+}
