4ef216c6b013d5f2e2964066fff54501a9fa7876
emmysteven <hello@emmysteven.com>
2023-04-27 12:59:52 +0100

    feat: add field for groups

--- src/Domain/Entities/Todo.cs
+++ src/Domain/Entities/Todo.cs
@@ -1,10 +1,18 @@
 using Checklist.Domain.Common;
+using Checklist.Domain.Enums;
 
 namespace Checklist.Domain.Entities;
 
 public class Todo: AuditableEntity//, IHasDomainEvent
 {
-    public string Name { get; set; } = string.Empty;
-    
+    public string Name { get; set; }
+    public Groups Group { get; set; }
+
+    public Todo(string name, Groups group)
+    {
+        Name = name;
+        Group = group;
+    }
+
     // public List<DomainEvent> DomainEvents { get; set; } = new List<DomainEvent>();
 }
