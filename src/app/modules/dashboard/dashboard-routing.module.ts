1b7709b493ceb9ddf0f7454205b28ca4a52bd420
emmysteven <hello@emmysteven.com>
2023-04-29 02:27:06 +0100

    feat: protect dashboard from unauthorized access

--- src/app/modules/dashboard/dashboard-routing.module.ts
+++ src/app/modules/dashboard/dashboard-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from "@app/modules/auth/layout.component";
 import { AddItemComponent } from "./add-item.component";
 import { GetItemComponent } from "./get-item.component";
+import { AuthGuard } from "@app/core/guards";
 
 const routes: Routes = [
   {
-    path: 'dashboard', component: LayoutComponent,
+    path: 'dashboard', component: LayoutComponent, canActivate: [AuthGuard],
     children: [
       { path: 'add_item', component: AddItemComponent },
       { path: 'report/detail', component: GetItemComponent }
