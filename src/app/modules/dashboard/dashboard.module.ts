1118de95c3eb60d76591a9b37595896815d61b72
emmysteven <hello@emmysteven.com>
2023-05-01 14:46:51 +0100

    build: install @fortawesome/angular-fontawesome

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -5,10 +5,12 @@ import { ReactiveFormsModule } from "@angular/forms";
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { AddItemComponent } from './add-item.component';
 import { GetItemComponent } from './get-item.component';
+import { FinalItemComponent } from './final-item.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 @NgModule({
-  declarations: [AddItemComponent, GetItemComponent],
-  imports: [CommonModule, ReactiveFormsModule, DashboardRoutingModule]
+  declarations: [AddItemComponent, GetItemComponent, FinalItemComponent],
+  imports: [CommonModule, ReactiveFormsModule, DashboardRoutingModule, FontAwesomeModule]
 })
 export class DashboardModule { }
