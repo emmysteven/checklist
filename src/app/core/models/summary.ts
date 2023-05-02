69fe3d3c45018961b1fa5d62b265d031aa3f2c52
emmysteven <hello@emmysteven.com>
2023-05-02 15:44:03 +0100

    add type for summary

---
+++ src/app/core/models/summary.ts
@@ -0,0 +1,11 @@
+export interface Summary {
+  id: string;
+  makers: string;
+  checkers: string;
+  dba: string;
+  startTime: string;
+  endTime: string;
+  eodDate: string;
+  txnCount: string;
+  duration: string;
+}
