50d41e65e2523783eb0ebbdabc67e187ec792305
emmysteven <hello@emmysteven.com>
2023-05-02 07:52:02 +0100

    feat: add component to view summary

---
+++ src/app/modules/dashboard/summary.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { AlertService, ItemService } from "@app/core/services";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-summary',
+  templateUrl: './summary.component.html',
+  styles: [
+  ]
+})
+export class SummaryComponent implements OnInit {
+
+  form: FormGroup = new FormGroup({});
+  data: any;
+  summary: any;
+  eodDate: string = '';
+  faMagnifyingGlass = faMagnifyingGlass;
+
+  loading = false;
+  submitted = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private alertService: AlertService,
+    private itemService: ItemService
+  ) { }
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      eodDate: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.alertService.clear();
+
+    const eodDate = this.form.value.eodDate;
+
+    console.log(eodDate)
+
+    if (this.form.invalid) { return console.log('Invalid Inputs') }
+
+    this.loading = true;
+
+    this.itemService.getSummary(eodDate).subscribe({
+      next: data => {
+        this.loading = false
+        this.summary = data
+        this.alertService.success(this.summary)
+      },
+      error: err => {
+        this.loading = false
+        this.alertService.error(err)
+        console.log(err)
+      }
+    });
+  }
+
+
+}
