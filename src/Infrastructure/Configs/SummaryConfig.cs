e301036485faa54f08449cd1bc0b6b85fde8a25d
emmysteven <hello@emmysteven.com>
2023-05-03 12:48:48 +0100

    add an EF configuration for summary

---
+++ src/Infrastructure/Configs/SummaryConfig.cs
@@ -0,0 +1,23 @@
+using Checklist.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Checklist.Infrastructure.Configs;
+
+public class SummaryConfig : IEntityTypeConfiguration<Summary>
+{
+    public void Configure(EntityTypeBuilder<Summary> builder)
+    {
+        builder.HasKey(s => s.Id);
+
+        builder.Property(s => s.Makers).HasMaxLength(50).IsRequired();
+        
+        builder.Property(s => s.Checkers).HasMaxLength(50).IsRequired();
+        
+        builder.Property(s => s.Dbas).HasMaxLength(50).IsRequired();
+        
+        builder.Property(s => s.EodDate).IsRequired();
+        
+        builder.Property(s => s.TxnCount).IsRequired();
+    }
+}
