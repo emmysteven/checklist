d2f8619152bb41141c7dd74348242d7aac513d44
emmysteven <hello@emmysteven.com>
2023-05-03 12:45:49 +0100

    add a field for username

--- src/Domain/Entities/User.cs
+++ src/Domain/Entities/User.cs
@@ -7,6 +7,7 @@ public class User : AuditableEntity
 {
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
+    public string? Username { get; set; }
     public string? Email { get; set; }
     public string? Password { get; set; }
     public DateTime? PasswordReset { get; set; }
