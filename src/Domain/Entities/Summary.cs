103250af054d1e6e64e13a7816c004879ff9285d
emmysteven <hello@emmysteven.com>
2023-05-03 12:44:57 +0100

    add more fields

--- src/Domain/Entities/Summary.cs
+++ src/Domain/Entities/Summary.cs
@@ -1,19 +1,35 @@
+using Checklist.Domain.Common;
+
 namespace Checklist.Domain.Entities;
 
-public class Summary
+public class Summary  : AuditableEntity
 {
-    public string[] Maker { get; set; }
-    public string Checker { get; set; }
-    public string[] Dba { get; set; }
+    public string Makers { get; set; }
+    public string Checkers { get; set; }
+    public string Dbas { get; set; }
     public DateTime EodDate { get; set; }
+    public string StartTime { get; set; }
+    public string EndTime { get; set; }
+    public string Duration { get; set; }
     public int TxnCount { get; set; }
 
-    public Summary(string[] maker, string checker, string[] dba, DateTime eodDate, int txnCount)
-    {
-        Maker = maker;
-        Checker = checker;
-        Dba = dba;
+    public Summary(
+        string makers, 
+        string checkers, 
+        string dbas,
+        DateTime eodDate, 
+        string startTime,
+        string endTime,
+        string duration,
+        int txnCount
+        ) {
+        Makers = makers;
+        Checkers = checkers;
+        Dbas = dbas;
         EodDate = eodDate;
+        StartTime = startTime;
+        EndTime = endTime;
+        Duration = duration;
         TxnCount = txnCount;
     }
 }
