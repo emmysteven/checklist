aa2749764bfe9d8a28fa10a93527742ae1d29cf5
emmysteven <hello@emmysteven.com>
2023-05-04 09:00:29 +0100

    add more fields and make all mandatory

--- src/app/core/models/todo.ts
+++ src/app/core/models/todo.ts
@@ -2,6 +2,8 @@ export interface Todo {
   id: string;
   name: string;
   group: string;
-  startTime?: string;
-  endTime?: string;
+  startTime: string;
+  endTime: string;
+  status: string;
+  remark: string;
 }
