0abdbba2083a973875fa14ab9caae243e7cfc6c6
emmysteven <hello@emmysteven.com>
2023-05-04 13:57:55 +0100

    add field to capture remarks

--- src/Domain/Entities/Item.cs
+++ src/Domain/Entities/Item.cs
@@ -2,14 +2,15 @@ using Checklist.Domain.Common;
 
 namespace Checklist.Domain.Entities;
 
-public class Item : AuditableEntity
+public class Item : AuditEntity
 {
     public new int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string StartTime { get; set; } = string.Empty;
     public string EndTime { get; set; } = string.Empty;
     public DateTime? EodDate { get; set; }
-    
+    public string Remark { get; set; } = string.Empty;
+
     // private bool _done;
     // public bool Done
     // {
