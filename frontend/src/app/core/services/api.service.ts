8ae8b64a46cc3192759946922b37292f269c8827
emmysteven <hello@emmysteven.com>
2023-05-18 23:43:33 +0100

    refactor: give self descriptive names to functions and types

--- frontend/src/app/core/services/api.service.ts
+++ frontend/src/app/core/services/api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { environment } from "@env/environment";
-import { Item, Todo, Summary } from "@app/core/models";
+import { Check, Todo, Summary } from "@app/core/models";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 
@@ -25,16 +25,16 @@ export class ApiService {
       }))
   }
 
-  addItem(item: any) {
-    return this.http.post(`${this.baseUrl}api/item`, item);
+  addCheck(check: any) {
+    return this.http.post(`${this.baseUrl}api/check`, check);
   }
 
-  addFinalItem(finalItem: any) {
-    return this.http.post(`${this.baseUrl}api/summary`, finalItem);
+  addSummary(summary: any) {
+    return this.http.post(`${this.baseUrl}api/summary`, summary);
   }
 
-  getItem(eodDate: string): Observable<Item> {
-    return this.http.get<Item>(`${this.baseUrl}api/item?eodDate=${eodDate}`, httpOptions)
+  getCheck(eodDate: string): Observable<Check> {
+    return this.http.get<Check>(`${this.baseUrl}api/check?eodDate=${eodDate}`, httpOptions)
       .pipe(map((response: any) => {
         return response.data;
     }))
