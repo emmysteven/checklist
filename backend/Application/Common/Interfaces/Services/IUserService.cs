3f62717ebc4d04c3d5503f33da355db2b072ba85
emmysteven <hello@emmysteven.com>
2023-05-26 14:08:03 +0100

    refactor: add new blueprint for UserService

--- backend/Application/Common/Interfaces/Services/IUserService.cs
+++ backend/Application/Common/Interfaces/Services/IUserService.cs
@@ -1,15 +1,13 @@
 using Checklist.Application.Common.Wrappers;
-using Checklist.Application.DTOs.Account;
+using Checklist.Application.DTOs;
+using Checklist.Application.ViewModels;
 
 namespace Checklist.Application.Common.Interfaces.Services;
 
 public interface IUserService
 {
-    Task<Response<AuthResponse>> GetByIdAsync(int id);
-    Task<IEnumerable<AuthResponse>> GetAllAsync();
-    Task<Response<string?>> RegisterAsync(RegisterRequest request, string? origin);
-    AuthResponse AuthenticateAsync(AuthRequest request, string ipAddress);
-    Task<Response<string?>> VerifyEmailAsync(int id, string token);
-    Task ForgotPasswordAsync(ForgotPasswordRequest request, string? origin);
-    Task<Response<string?>> ResetPasswordAsync(ResetPasswordRequest request);
+    Task<Response<AuthVm>> GetByIdAsync(int id);
+    Task<IEnumerable<AuthVm>> GetUsersAsync();
+    Task<Response<string?>> AddUserAsync(UserDto dto);
+    AuthVm AuthenticateAsync(AuthDto authDto);
 }
