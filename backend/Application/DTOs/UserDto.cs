bec978008d1551bca6a9436754b6cd3bf0cd2256
emmysteven <hello@emmysteven.com>
2023-05-26 13:37:20 +0100

    refactor: rename to UserDto and rename fields

--- backend/Application/DTOs/UserDto.cs
+++ backend/Application/DTOs/UserDto.cs
@@ -1,14 +1,10 @@
-using System.ComponentModel.DataAnnotations;
 using Checklist.Domain.Enums;
 
 namespace Checklist.Application.DTOs;
 
-public class RegisterDto
+public class UserDto
 {
-    [Required] public string? FullName { get; set; }
-
-    [Required] public string? UserName { get; set; }
-
-    public Roles Role { get; set; }
-
+    public string? FullName { get; set; }
+    public string? UserName { get; set; }
+    public Roles? Role { get; set; }
 }
