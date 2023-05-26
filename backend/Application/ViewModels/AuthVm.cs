2c2127067e41a07dded7236d42c9b1f00b082b9d
emmysteven <hello@emmysteven.com>
2023-05-26 14:01:43 +0100

    refactor: rename to AuthVm

--- backend/Application/ViewModels/AuthVm.cs
+++ backend/Application/ViewModels/AuthVm.cs
@@ -1,12 +1,9 @@
-using System.Text.Json.Serialization;
-
 namespace Checklist.Application.ViewModels;
 
-public class AuthResponse
+public class AuthVm
 {
     public string? FullName { get; set; }
     public string? ResponseCode { get; set; }
     public string? ResponseMessage { get; set; }
     public string? Token { get; set; }
-    [JsonIgnore] public string? RefreshToken { get; set; }
 }
