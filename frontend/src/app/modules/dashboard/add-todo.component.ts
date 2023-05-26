a6b46a3b95f69e2ee230377b2e773242f7e6b282
emmysteven <hello@emmysteven.com>
2023-05-26 08:55:40 +0100

    feat: add component for adding todos

---
+++ frontend/src/app/modules/dashboard/add-todo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-todo',
+  templateUrl: './add-todo.component.html',
+  styles: [
+  ]
+})
+
+export class AddTodoComponent {
+
+}
