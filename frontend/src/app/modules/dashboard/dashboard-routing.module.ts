a41087069522cfd978a4a5c9b22565ad78a85d58
emmysteven <hello@emmysteven.com>
2023-05-27 11:17:37 +0100

    add routes for addTodo component and getTodo component

--- frontend/src/app/modules/dashboard/dashboard-routing.module.ts
+++ frontend/src/app/modules/dashboard/dashboard-routing.module.ts
@@ -6,11 +6,15 @@ import { GetCheckComponent } from "./get-check.component";
 import { AddSummaryComponent } from "./add-summary.component";
 import { GetSummaryComponent } from "./get-summary.component";
 import { AuthGuard } from "@app/core/guards";
+import { GetTodoComponent } from "./get-todo.component";
+import { AddTodoComponent } from "./add-todo.component";
 
 const routes: Routes = [
   {
     path: 'dashboard', component: LayoutComponent, canActivate: [AuthGuard],
     children: [
+      { path: 'todos/get_todos', component: GetTodoComponent },
+      { path: 'todos/add_todos', component: AddTodoComponent },
       { path: 'checks/add_checks', component: AddCheckComponent },
       { path: 'checks/add_summary', component: AddSummaryComponent },
       { path: 'report/get_check', component: GetCheckComponent },
