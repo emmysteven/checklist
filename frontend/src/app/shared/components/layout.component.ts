12d09ea19dbb68bb3dfc1a28bb7f41ba8068fa72
emmysteven <hello@emmysteven.com>
2023-05-27 10:53:48 +0100

    style: add more padding to the bottom of the page

--- frontend/src/app/shared/components/layout.component.ts
+++ frontend/src/app/shared/components/layout.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-layout',
   template: `
-    <div class="col-md-6 offset-md-3 mt-5">
+    <div class="col-md-6 offset-md-3 mt-5 mb-5">
       <router-outlet></router-outlet>
     </div>
   `,
   styles: [`
     .mt-5 {
-      padding-top: 4rem;
+      padding-top: 6rem;
+    }
+    .mb-5 {
+      padding-bottom: 2rem;
     }
   `]
 })
