9431fde05f26a710145b27d6eb74918819d9079f
emmysteven <hello@emmysteven.com>
2023-05-27 11:15:43 +0100

    index addTodo component and getTodo component

--- frontend/src/app/modules/dashboard/index.ts
+++ frontend/src/app/modules/dashboard/index.ts
@@ -2,4 +2,6 @@ export * from './dashboard.module';
 export * from './add-check.component';
 export * from './get-check.component';
 export * from './add-summary.component'
+export * from './add-todo.component'
+export * from './get-todo.component'
 export * from './dashboard-routing.module';
