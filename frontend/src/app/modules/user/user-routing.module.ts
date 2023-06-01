dad05ac9161bfa3939dd5defcc0ab9c667818d4f
emmysteven <hello@emmysteven.com>
2023-06-01 22:02:27 +0100

    add LoginGuard to login route

--- frontend/src/app/modules/user/user-routing.module.ts
+++ frontend/src/app/modules/user/user-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from "./login.component";
-import { AddUserComponent } from "./add-user.component";
+import { LoginGuard } from "@app/core/guards/login.guard";
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
-  { path: 'user/add', component: AddUserComponent }
+  { path: '', component: LoginComponent, canActivate: [LoginGuard] },
+  // { path: 'user/add', component: AddUserComponent }
 ];
 
 @NgModule({
