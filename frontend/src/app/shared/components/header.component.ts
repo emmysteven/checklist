753820e0926f1641335e9322689825a6f34f85cf
emmysteven <hello@emmysteven.com>
2023-06-01 21:37:03 +0100

    feat: display nav links according to user role

--- frontend/src/app/shared/components/header.component.ts
+++ frontend/src/app/shared/components/header.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { AuthService } from "@app/core/services";
+import { UserService } from "@app/core/services";
 
 @Component({
   selector: 'app-header',
@@ -23,30 +23,38 @@ import { AuthService } from "@app/core/services";
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-<!--            <li *ngIf="!isLoggedIn()" class="nav-item">-->
-<!--              <a class="nav-link" routerLink="/login">Login</a>-->
-<!--            </li>-->
-<!--            <li *ngIf="!isLoggedIn()" class="nav-item">-->
-<!--              <a class="nav-link" routerLink="/register">Register</a>-->
-<!--            </li>-->
+            <li *ngIf="isLoggedIn() && isAdmin()" class="nav-item me-2 dropdown">
+              <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">User</a>
+              <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" routerLink="/dashboard/users/add">Add User</a></li>
+                <li><a class="dropdown-item" routerLink="/dashboard/users">View Users</a></li>
+              </ul>
+            </li>
+
+            <li *ngIf="isLoggedIn() && isChecker()" class="nav-item me-2 dropdown">
+              <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Todos</a>
+              <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" routerLink="/dashboard/todos/add">Add Todo</a></li>
+                <li><a class="dropdown-item" routerLink="/dashboard/todos">Todos</a></li>
+              </ul>
+            </li>
+
             <li *ngIf="isLoggedIn()" class="nav-item me-2 dropdown">
               <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Reports</a>
               <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" routerLink="/dashboard/report/get_check">Checks</a></li>
-                <li><a class="dropdown-item" routerLink="/dashboard/report/get_summary">Summary</a></li>
-                <li><hr class="dropdown-divider" /></li>
-                <li><a class="dropdown-item" routerLink="/dashboard/report/get_todo">Todos</a></li>
+                <li><a class="dropdown-item" routerLink="/dashboard/checks">Checks</a></li>
+                <li><a class="dropdown-item" routerLink="/dashboard/summary">Summary</a></li>
               </ul>
             </li>
-            <li *ngIf="isLoggedIn()" class="nav-item me-2 dropdown">
+
+            <li *ngIf="isLoggedIn() && isMaker()" class="nav-item me-2 dropdown">
               <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Checks</a>
               <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" routerLink="/dashboard/checks/add_checks">Add Checks</a></li>
-                <li><a class="dropdown-item" routerLink="/dashboard/checks/add_summary">Add Summary</a></li>
-                <li><hr class="dropdown-divider" /></li>
-                <li><a class="dropdown-item" routerLink="/dashboard/report/add_todo">Add Todo</a></li>
+                <li><a class="dropdown-item" routerLink="/dashboard/checks/add">Add Checks</a></li>
+                <li><a class="dropdown-item" routerLink="/dashboard/summary/add">Add Summary</a></li>
               </ul>
             </li>
+
             <li *ngIf="isLoggedIn()" class="nav-item">
               <a class="nav-link" role="button" (click)="logout()">Logout</a>
             </li>
@@ -59,10 +67,16 @@ import { AuthService } from "@app/core/services";
 
 
 export class HeaderComponent implements OnInit {
+  userRole: string | null = '';
+  constructor(public userService: UserService) {}
 
-  constructor(public authService: AuthService) {}
-  isLoggedIn = ():boolean => this.authService.isLoggedIn();
-  logout = ():void => this.authService.logout();
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.userRole = this.userService.getUserRole();
+  }
 
+  isChecker = ():boolean => this.userService.isChecker(this.userRole);
+  isMaker = ():boolean => this.userService.isMaker(this.userRole);
+  isAdmin = ():boolean => this.userService.isAdmin(this.userRole);
+  isLoggedIn = ():boolean => this.userService.isLoggedIn();
+  logout = ():void => this.userService.logout();
 }
