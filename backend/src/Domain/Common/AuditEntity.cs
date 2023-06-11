607273efd7663a3383327c937805bb845237ec9b
emmysteven <hello@emmysteven.com>
2023-06-11 08:05:37 +0100

    refactor: make MakerId nullable

--- backend/src/Domain/Common/AuditEntity.cs
+++ backend/src/Domain/Common/AuditEntity.cs
@@ -3,7 +3,7 @@ namespace Checklist.Domain.Common;
 public class AuditEntity
 {
     public long Id { get; set; }
-    public string MakerId { get; set; } = string.Empty;
+    public string? MakerId { get; set; } = string.Empty;
     public DateTime? MakerDt { get; set; }
     public string CheckerId { get; set; } = string.Empty;
     public DateTime? CheckerDt { get; set; }
