8bb955f73f82adeb9e6421c83e6f51b4899ee107
emmysteven <hello@emmysteven.com>
2023-06-13 12:22:22 +0100

    refactor: provide implementation for authorizing checks

--- backend/src/Infrastructure/Services/CheckService.cs
+++ backend/src/Infrastructure/Services/CheckService.cs
@@ -7,6 +7,7 @@ using Checklist.Application.ViewModels;
 using Checklist.Domain.Entities;
 using Checklist.Infrastructure.Helpers;
 using Checklist.Infrastructure.Persistence;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
 namespace Checklist.Infrastructure.Services;
@@ -14,12 +15,14 @@ namespace Checklist.Infrastructure.Services;
 public class CheckService : ICheckService
 {
     private readonly IMapper _mapper;
+    private readonly DataContext _context;
     private readonly DbSet<Check> _check;
     private readonly ICurrentUserService _currentUser;
 
     public CheckService(IMapper mapper, DataContext context, ICurrentUserService currentUser)
     {
         _mapper = mapper;
+        _context = context;
         _currentUser = currentUser;
         _check = context.Set<Check>();
     }
@@ -78,6 +81,35 @@ public class CheckService : ICheckService
             _check.Update(updatedCheck);
         }
         return checkDto.Checks.FirstOrDefault()!;
+    }
+    
+    public async Task<Response<IEnumerable<CheckVm>>> CheckedAsync(CheckedDto checkedDto)
+    {
+        var dateValue = IpHelper.GetDate(checkedDto.EodDate);
+        var isCheck = _check.Any(x => x.EodDate == dateValue);
+        if (isCheck)
+        {
+            const string sql = @"
+            UPDATE [corebanking].[dbo].[Chk_Check]
+            SET CheckerId = @CheckerId,
+                CheckerDt = @CheckerDt,
+                AuthStatus = @AuthStatus
+            WHERE EodDate = @EodDate";
+
+            object[] parameters = {
+                new SqlParameter("@CheckerId", _currentUser.Username!),
+                new SqlParameter("@CheckerDt", DateTime.UtcNow),
+                new SqlParameter("@AuthStatus", true),
+                new SqlParameter("@EodDate", dateValue)
+            };
+
+            await _context.Database.ExecuteSqlRawAsync(sql, parameters);
+
+            var checks = await _check.Where(x => x.EodDate == dateValue).ToListAsync();
+            var checkVm = _mapper.Map<IEnumerable<CheckVm>>(checks);
+            return new Response<IEnumerable<CheckVm>>(checkVm);
+        }
+        return new Response<IEnumerable<CheckVm>>(Enumerable.Empty<CheckVm>(), "No checks found");
     }
     
     
