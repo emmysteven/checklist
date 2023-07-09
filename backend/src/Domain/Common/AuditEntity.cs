7d857e8d7daacbbabdd9cd76bcfe43800c96fe2c
emmysteven <hello@emmysteven.com>
2023-07-09 19:25:32 +0100

    refactor: make CheckerId and AuthStatus nullable

--- backend/src/Domain/Common/AuditEntity.cs
+++ backend/src/Domain/Common/AuditEntity.cs
@@ -5,7 +5,7 @@ public class AuditEntity
     public long Id { get; set; }
     public string? MakerId { get; set; } = string.Empty;
     public DateTime? MakerDt { get; set; }
-    public string CheckerId { get; set; } = string.Empty;
+    public string? CheckerId { get; set; } = string.Empty;
     public DateTime? CheckerDt { get; set; }
-    public bool? AuthStatus { get; set; }
+    public bool? AuthStatus { get; set; } = false;
 }
